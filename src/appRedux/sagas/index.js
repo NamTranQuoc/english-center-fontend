cde0ecac7459bbf156ea01bd596d76e8fd547755
NamTran <nam.tran@zamo.io>
2021-11-04 17:15:53 +0700

    fix loop api get list

--- src/appRedux/sagas/index.js
+++ src/appRedux/sagas/index.js
@@ -1,10 +1,12 @@
 import {all} from "redux-saga/effects";
 import authSagas from "./Auth";
 import memberSagas from "./Member"
+import settingsSagas from "./Settings"
 
 export default function* rootSaga(getState) {
   yield all([
     authSagas(),
     memberSagas(),
+    settingsSagas(),
   ]);
 }
