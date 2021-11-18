03ac9b503d5503f32686aa06076f5f789996a006
NamTran <nam.tran@zamo.io>
2021-11-18 23:11:48 +0700

    thêm editor

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -1,23 +1,10 @@
 import React from "react";
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import WYSISWYG from "./WYSISWYG";
 
-const Editor = () => {
+const MyEditor = () => {
     return (
-        <div>
-            <h2>Using CKEditor 5 build in React</h2>
-            <CKEditor
-                editor={ ClassicEditor }
-                config={{cloudServices: {
-                        tokenUrl: 'https://84898.cke-cs.com/token/dev/9b0051a3ba6a6855f05199dc5d2c804f49c4145e6b9c30c08c426df9e93e',
-                        uploadUrl: 'https://84898.cke-cs.com/easyimage/upload/'
-                    }}}
-                onChange={ ( event, editor ) => {
-                    console.log(editor.getData());
-                }}
-            />
-        </div>
+        <WYSISWYG />
     );
 };
 
-export default Editor;
+export default MyEditor;
