fd70b6728f3aea012bb013e3cff2252ed832bcd7
QuocNam <namtranquoc322@gmail.com>
2022-07-18 15:31:59 +0700

    host

--- src/appRedux/store/Host.js
+++ src/appRedux/store/Host.js
@@ -1,2 +1,2 @@
-export const host = "http://localhost:8080";
-// export const host = "https://ec-pmp.herokuapp.com";
+// export const host = "http://localhost:8080";
+export const host = "https://ec-pmp.herokuapp.com";
